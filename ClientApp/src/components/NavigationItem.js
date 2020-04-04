1475b0a155fa45c3b6042e1979386dfcde927e9b
Stephen Burden <test.actemium@LighthouseSystems.com>
2020-04-04 18:40:39 +0100

    Active navigation item bug

--- ClientApp/src/components/NavigationItem.js
+++ ClientApp/src/components/NavigationItem.js
@@ -4,13 +4,12 @@ import './css/NavigationItem.css'
 export default class NavigationItem extends React.Component {
     constructor(props){
         super(props);
-        this.state = {name: props.name, class: props.isActive === 1 ? "blog-nav-item active" : "blog-nav-item"};
+        this.state = {name: props.name, class: props.isActive === "1" ? "blogNavItem active" : "blogNavItem"};
     }
 
     render() {
         return (
-            <a className="blogNavItem" href="#">{this.state.name}</a>
+            <a className={this.state.class} href="#">{this.state.name}</a>
         );
     }
-
 }
